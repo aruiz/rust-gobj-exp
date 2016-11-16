ed7e8feebecc2786ca8d332cad23dfd2ffb8c86f
Niko Matsakis <niko@alum.mit.edu>
2016-11-16 11:06:39 -0500

    add some methods

--- rust-gobject/src/ptr.rs
+++ rust-gobject/src/ptr.rs
@@ -1,9 +1,27 @@
+use std::ops::Deref;
+
 use gobject;
 use gtypes;
 
 /// Represents an owned gobject `T`
 pub struct Ptr<T> {
     data: *mut T
+}
+
+impl<T> Ptr<T> {
+    pub unsafe fn with_ptr(data: *mut T) -> Ptr<T> {
+        Ptr { data: data }
+    }
+}
+
+impl<T> Deref for Ptr<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe {
+            &(*self.data)
+        }
+    }
 }
 
 impl<T> Drop for Ptr<T> {
